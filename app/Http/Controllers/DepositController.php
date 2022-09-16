0cfe0a68fa38466a92085379c8790885893a9fa8
Funke Faith Olasupo <fizzyflexy397@gmail.com>
2022-09-16 17:24:27 +0100

    database notification: basic setup

---
+++ app/Http/Controllers/DepositController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Deposit;
+use App\Models\User;
+use App\Notifications\DepositSuccessful;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class DepositController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function deposit(Request $request){
+
+        $deposit = Deposit::create([
+            'user_id' =>Auth::user()->id,
+            'amount'  => $request->amount
+        ]);
+
+        User::find(Auth::user()->id)->notify(new DepositSuccessful($deposit->amount));
+        
+        return redirect()->back()->with('status','Your deposit was successful!');
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success text-center" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <form method="POST" action="{{ route('deposit') }}">
+                        @csrf
+                        <h5 class="text-center mb-3">Make A Deposit</h5>
+
+                        <div class="row mb-3">
+                            <label for="amount" class="col-md-4 col-form-label text-md-end">{{ __('Amount') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="amount" type="number" class="form-control @error('amount') is-invalid @enderror" name="amount" value="{{ old('amount') }}" required autocomplete="amount" autofocus>
+
+                                @error('amount')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Deposit') }}
+                                </button>
+
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
